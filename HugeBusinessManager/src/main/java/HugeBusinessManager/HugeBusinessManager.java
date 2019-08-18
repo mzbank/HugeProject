f32ee6a33462a66e925a5dc2fe902f9b939147a2
mzbank <2536684936@qq.com>
2019-08-18 23:49:32 +0800

    项目整体框架构建

---
+++ HugeBusinessManager/src/main/java/HugeBusinessManager/HugeBusinessManager.java
@@ -0,0 +1,12 @@
+package HugeBusinessManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugeBusinessManager {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HugeBusinessManager.class, args);
+    }
+}
